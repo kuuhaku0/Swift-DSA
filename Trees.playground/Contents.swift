68913323955aa4d8a8b361e2a29966c834e2e316
Tyler Zhao <tylerzhao@ac.c4q.nyc>
2018-02-25 20:29:18 -0500

    add BFT, DFT, implement BST using Queue using LinkedList

--- Trees.playground/Contents.swift
+++ Trees.playground/Contents.swift
@@ -2,111 +2,207 @@
 
 import Cocoa
 
-class BinaryTree {
-    var left: BinaryTree?
-    var root: Int?
-    var right: BinaryTree?
-    init(_ root: Int) {
-        self.root = root
+// LinkedList Node
+class Node<T> {
+    var key: T
+    var next: Node?
+    init(key: T) {
+        self.key = key
     }
-    
-    public func insert(value: Int) {
-        guard let root = root else {return}
-        
-        if value <= root {
-            if left == nil {
-                left = BinaryTree(value)
-            } else {
-                left?.insert(value: value)
-            }
-        } else {
-            if right == nil {
-                right = BinaryTree(value)
-            } else {
-                right?.insert(value: value)
-            }
+}
+
+// LinkedList for Queue
+class LinkedList<T> {
+    public var head: Node<T>?
+    public var count: Int {
+        guard head != nil else {return 0}
+        var currentNode = head
+        var count = 0
+        while currentNode != nil {
+            count += 1
+            currentNode = currentNode?.next
         }
+        return count
+    }
+    public var isEmpty: Bool {return self.head == nil}
+    public func append(element node: Node<T>) {
+        guard head != nil else {
+            head = node
+            return
+        }
+        var currentNode = head
+        while currentNode?.next != nil {
+            currentNode = currentNode?.next
+        }
+        currentNode?.next = node
+    }
+    public func dropFirst() -> T? {
+        guard let oldHead = head else {return nil}
+        self.head = oldHead.next
+        return oldHead.key
+    }
+}
+
+// Queue for Breadth first traversal
+struct Queue<T> {
+    private var arr = LinkedList<T>()
+    public var count: Int {return arr.count}
+    public var isEmpty: Bool {return arr.isEmpty}
+    
+    public mutating func enQueue(_ element: T) {
+        arr.append(element: Node(key: element))
+    }
+    public mutating func deQueue() -> T? {
+        guard !arr.isEmpty else {return nil}
+        return arr.dropFirst()
+    }
+    public func peek() -> T? {
+        return arr.head?.key
+    }
+}
+
+// Binary Tree
+enum Order {
+    case inOrder
+    case preOrder
+    case postOrder
+}
+
+class NumberTreeNode {
+    var key: Int
+    var left: NumberTreeNode?
+    var right: NumberTreeNode?
+    init(key: Int) {
+        self.key = key
+    }
+    public func insert(_ value: Int) {
+        if value <= key { // left side
+            if left == nil {left = NumberTreeNode(key: value)} else {left?.insert(value)}}
+        else { //value > key, right side
+            if right == nil {right = NumberTreeNode(key: value)}
+            else {right?.insert(value)}}
     }
     
     public func contains(value: Int) -> Bool {
-        guard let root = root else {return false}
-        if value == root {
-            return true
-        } else if value < root {
-            if left == nil {
-                return false
-            } else {
-                return left!.contains(value: value)
-            }
-        } else {
-            if right == nil {
-                return false
-            } else {
-                return right!.contains(value:value)
-            }
-        }
+        if value == key { return true } else if value < key {
+            if left == nil { return false }
+            else {return left!.contains(value: value)}} // go down a level on left
+            
+        else { //value less than key
+            if right == nil {return false}
+            else {return right!.contains(value:value)}} // go down a level on right
     }
-    
-    public func printInOrder() {
-        guard let root = root else {return}
+    public func printInOrder(order: Order) {
         //preorder
-        print(root)
-        if left != nil {
-            left?.printInOrder()
+        switch order {
+        case .preOrder:
+            print(key)
+            left?.printInOrder(order: .preOrder)
+            right?.printInOrder(order: .preOrder)
+        case .inOrder:
+            left?.printInOrder(order: .inOrder)
+            print(key)
+            right?.printInOrder(order: .inOrder)
+        case .postOrder:
+            left?.printInOrder(order: .postOrder)
+            right?.printInOrder(order: .postOrder)
+            print(key)
         }
-        //inorder
-        //print(root)
-        if right != nil {
-            right?.printInOrder()
-        }
-        //postorder
-        //print(root)
-        
+    }
+}
+
+class BinaryTreeNode<T> {
+    var key: T
+    var left: BinaryTreeNode?
+    var right: BinaryTreeNode?
+    init(key: T) {
+        self.key = key
     }
     
-    //    public func delete(value: Int) {
-    //        guard let root = root else {return}
-    ////        if root == value {
-    ////            if left == nil && right == nil {
-    ////                self.root = nil
-    ////            } else if left == nil {
-    ////
-    ////            }
-    ////        }
-    //        if value < root {
-    //            guard let left = left else {return}
-    //            if left.root == value && left.left == nil && left.right == nil {
-    //                left.root = nil
-    //            } else if left.root == value && left.left == nil {
-    //
-    //            }
-    //        } else if value > root {
-    //            right?.delete(value: value)
-    //        }
-    //    }
+    public func preOrderDepthFirstTraversal() {
+        left?.preOrderDepthFirstTraversal()
+        print(key)
+        right?.preOrderDepthFirstTraversal()
     }
+    public func inOrderDepthFirstTraversal() {
+        print(key)
+        left?.inOrderDepthFirstTraversal()
+        right?.inOrderDepthFirstTraversal()
+    }
+    public func postOrderDepthFirstTraversal() {
+        left?.postOrderDepthFirstTraversal()
+        right?.postOrderDepthFirstTraversal()
+        print(key)
+    }
+}
 
-    var root = BinaryTree(5)
-    root.insert(value: 2)
-    root.insert(value: 3)
-    root.insert(value: 4)
-    root.insert(value: 1)
-    root.insert(value: 10)
-    root.insert(value: 7)
-    root.insert(value: 6)
-    root.insert(value: 8)
-    root.insert(value: 12)
-    root.insert(value: 12)
+class BinaryTree<T> {
+    var root: BinaryTreeNode<T>?
+    var numRoot: NumberTreeNode?
     
-    root.printInOrder()
+    public func breadthFirstTraversal() {
+        guard let root = self.root else {return}
+        var processQueue = Queue<BinaryTreeNode<T>>()
+        processQueue.enQueue(root) // enQueue root node to execute loop
+        while !processQueue.isEmpty {
+            let currentNode = processQueue.deQueue()! // returns BinaryTreeNode of each level from left to right
+            print(currentNode.key)
+            if let leftNode = currentNode.left { // if there is more left node
+                processQueue.enQueue(leftNode)
+            }
+            if let rightNode = currentNode.right { // if there is more right node
+                processQueue.enQueue(rightNode)
+            }
+        }
+    }
+    public func depthFirstTraversal(in order: Order) {
+        guard let root = root else {return}
+        switch order {
+        case .preOrder:
+            root.preOrderDepthFirstTraversal()
+        case .inOrder:
+            root.inOrderDepthFirstTraversal()
+        case .postOrder:
+            root.postOrderDepthFirstTraversal()
+        }
+    }
+}
+//BinaryTreeNode
+let tree = BinaryTree<Int>()
+let root = BinaryTreeNode(key: 0)
+let nodeOne = BinaryTreeNode(key: 1)
+let nodeTwo = BinaryTreeNode(key: 2)
+let nodeThree = BinaryTreeNode(key: 3)
+let nodeFour = BinaryTreeNode(key: 4)
+let nodeFive = BinaryTreeNode(key: 5)
+let nodeSix = BinaryTreeNode(key: 6)
 
-//let root = TreeNode(1)
-//let nodeTwo = TreeNode(2)
-//let nodeThree = TreeNode(3)
-//let nodeFour = TreeNode(4)
-//let nodeFive = TreeNode(5)
+tree.root = root
+root.left = nodeOne
+root.right = nodeTwo
+nodeOne.left = nodeThree
+nodeOne.right = nodeFour
+nodeFour.right = nodeSix
+nodeTwo.right = nodeFive
+tree.breadthFirstTraversal()
+//print()
+//tree.depthFirstTraversal(in: .inOrder)
+
+/************************************/
+//NumberTreeNode that contain only Integers
+//Call insert function to automatically insert in sorted order of BST
+
+let numTree = BinaryTree<Int>()
+let numRoot = NumberTreeNode(key: 1)
+numTree.numRoot = numRoot
+numTree.numRoot?.insert(2)
+numTree.numRoot?.insert(3)
+numTree.numRoot?.insert(6)
+numTree.numRoot?.insert(4)
+numTree.numRoot?.insert(9)
+numTree.numRoot?.insert(11)
 //
-//root.left = nodeTwo
-//root.right = nodeThree
-//nodeTwo.left = nodeFour
-//nodeTwo.right = nodeFive
+//numTree.numRoot?.contains(value: 9)
+//print("numTree")
+//numTree.numRoot?.printInOrder(order: .inOrder)
+
